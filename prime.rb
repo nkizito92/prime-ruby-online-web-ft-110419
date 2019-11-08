616da28c5d1a1e5432d4811002df7e90cb08e860
Kizito Njoku <nkizito57@gmail.com>
2019-11-08 20:49:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,13 @@
 # Add  code here!
 
 def prime?(num)
+  # returns false if the number is not a integer
   return false if !num.integer?
   
   return false if num < 2 
   
   return true if num == 2 
-  
+  # returns false if the number dividual is equal to 0
   (2..num-1).each {|int| return false if num % int == 0}
   
   true 
